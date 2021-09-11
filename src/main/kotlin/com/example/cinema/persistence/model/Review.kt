0c3582faf6a6f20837509aa1b706a450cf07a375
Jorge Rodriguez <jorge.rodriguez1@grainger.com>
2021-09-11 01:27:46 -0500

    Overrides method to avoid having circular dependencies

--- src/main/kotlin/com/example/cinema/persistence/model/Review.kt
+++ src/main/kotlin/com/example/cinema/persistence/model/Review.kt
@@ -1,11 +1,10 @@
 package com.example.cinema.persistence.model
 
-import java.math.BigDecimal
 import javax.persistence.*
 
 @Entity
 @Table(name = "review")
-data class Review(
+class Review(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int,
@@ -17,4 +16,28 @@ data class Review(
     @ManyToOne()
     @JoinColumn(name = "movie_id", nullable = false)
     val movie: Movie
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Review
+
+        if (id != other.id) return false
+        if (rate != other.rate) return false
+        if (comment != other.comment) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + rate
+        result = 31 * result + comment.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Review(id=$id, rate=$rate, comment='$comment')"
+    }
+}
--- src/main/kotlin/com/example/cinema/persistence/model/User.kt
+++ src/main/kotlin/com/example/cinema/persistence/model/User.kt
@@ -4,7 +4,7 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "\"user\"")
-data class User(
+class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int?,
@@ -14,4 +14,30 @@ data class User(
     val role: Role,
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private val reviews: Set<Review> = hashSetOf()
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (id != other.id) return false
+        if (username != other.username) return false
+        if (password != other.password) return false
+        if (role != other.role) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id ?: 0
+        result = 31 * result + username.hashCode()
+        result = 31 * result + password.hashCode()
+        result = 31 * result + role.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "User(id=$id, username='$username', role=$role)"
+    }
+}
